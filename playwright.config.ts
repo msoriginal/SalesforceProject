f5df0a6667360f809102dd2a92d28313a834547b
Mukesh Sharma <oom06o7k5uuyexb74ymg5lquzfepcd@bots.bitbucket.org>
2025-07-25 23:28:33 +0530

    updated yml and config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,6 +31,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     screenshot: 'only-on-failure',
     trace: 'on-first-retry',
+    headless: true
     
   },
 
