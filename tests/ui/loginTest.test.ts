17659b4b105e0ff68bbf8bc029b33ddfa5ffd127
Mukesh Sharma <oom06o7k5uuyexb74ymg5lquzfepcd@bots.bitbucket.org>
2025-07-26 13:13:03 +0530

    ignored password login

--- tests/ui/loginTest.test.ts
+++ tests/ui/loginTest.test.ts
@@ -4,14 +4,14 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-test('Login test', async ({page}) => {
-    const loginPage = new LoginPage(page);
-    await page.goto (`${process.env.SFDC_BASE_URL}/login.jsp`);
-    await loginPage.login(process.env.SFDC_USERNAME!, process.env.SFDC_PASSWORD!);
-    await page.waitForTimeout(10000);
-    expect(await page.title()).toBe('Home | Salesforce');
+// test('Login test', async ({page}) => {
+//     const loginPage = new LoginPage(page);
+//     await page.goto (`${process.env.SFDC_BASE_URL}/login.jsp`);
+//     await loginPage.login(process.env.SFDC_USERNAME!, process.env.SFDC_PASSWORD!);
+//     await page.waitForTimeout(10000);
+//     expect(await page.title()).toBe('Home | Salesforce');
     
-})
+// })
 
 test('OAuth Login test', async ({page}) => {
     const loginPage = new LoginPage(page);
